370705684b76c33b5a640dc49adc5522de8b6cca
AdobeCCing <jinyou0196@gmail.com>
2018-09-18 15:23:02 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Login System</title>
+<link rel="stylesheet" type="text/css" href="cisproject.css">
+</head>
+
+<body>
+	
+<header>
+	<nav>
+		<div class="main-wrapper">
+		  <ul>
+			<li><a href="index.php">Home</a></li>
+			</ul>
+		<div class="nav-login">
+			<form>
+				<input type="text" name="uid" placeholder="username/email">
+				<input type="password" name="password" placeholder="password">
+				<button type="submit" name="submit">Login</button>
+			</form>
+			<a href="signup.php">Sign up</a>
+		</div>
+		</div>
+	</nav>
+</header>
+	
+<section>
+	
+</section>
+</body>
+</html>
